ba7ad3bf92d7ca523dd301600a825f859e11785f
AlisonWonderland <dimitridawn@gmail.com>
2019-08-31 21:36:29 -0700

    Moved these files

--- Github Profile Stats/public/js/sockets.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-var repos_ul = document.querySelector(".repos");
-
-console.log("hi");
-socket.on('connected', function(msg) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(msg));
-    repos_ul.appendChild(li);
-});
