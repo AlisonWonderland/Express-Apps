4ea65f42d8f31abf4bc7e3beaac4ce05fa6343c4
AlisonWonderland <dimitridawn@gmail.com>
2019-09-04 13:18:06 -0700

    Added hard coded percentage bar

--- Github Profile Stats/public/js/welcome.js
+++ Github Profile Stats/public/js/welcome.js
@@ -1,32 +1,52 @@
 var socket = io();
 var repos_ul = document.querySelector(".repos");
-var repo_container = document.querySelector(".repo-container");
+var repos_container = document.querySelector(".repos-container");
+var color_list = ['#ffffb1', '#b1ffb1', '#ffb265', '#b1b1ff', '#ffd8b1', '#b1ffff', '#fefeff', '#a343ff']
 
 // Add the repo info to the page
 socket.on('repo info received', function(data) {
-    var div = document.createElement("div");
+    var info_div = document.createElement("div");
     var h2 = document.createElement("h2");
     var span = document.createElement("span");
 
     h2.innerText = data.repo_name;
-    div.appendChild(h2);
+    h2.classList.add("repo-name");
+    info_div.appendChild(h2);
 
     var languages = Object.keys(data.languageData); 
     var percentages = Object.values(data.languageData);
 
     if(languages.length === 0) {
         span.innerText = "No code in this repo/Empty repo";
-        div.appendChild(span);
+        span.classList.add("repo-language");
+        info_div.appendChild(span);
     }
 
     else {
         for(var i = 0; i < languages.length; ++i) {
+            // Maybe put this in a function
             span = document.createElement("span"); 
             span.innerText = languages[i] + " " +  percentages[i] + "%\n";
-            div.appendChild(span);
+            span.classList.add("repo-language");
+            info_div.appendChild(span);
+            // Add bar. Currently adding pars to webpage
+            info_div.append(addPercentBar(percentages[i]));
         }
     }
     
-    div.classList.add("repo-info");
-    repo_container.appendChild(div);
+    info_div.classList.add("repo-info");
+    repos_container.appendChild(info_div);
 });
+
+function addPercentBar(percentage) {
+    var whole_bar = document.createElement("div");
+    whole_bar.setAttribute("id", "whole-bar");
+
+    var percentage_bar = document.createElement("div");
+    percentage_bar.setAttribute("id", "percentage-bar");
+    // percentage.setAttribute("style");
+
+    whole_bar.appendChild(percentage_bar);
+
+    return whole_bar;
+}
